4401bcad074651fbf8baa532ceeec1fe25303cb9
DONGHOON JOEN <dhjeon1993@gmail.com>
2021-05-04 15:17:49 +0900

    create product module

---
+++ microservices/product/src/test/java/jeon/donghoon/micro/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package jeon.donghoon.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
