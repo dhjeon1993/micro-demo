9ed9cf7d1bccb1213a95d9a687bcc222ef8b721f
DONGHOON JOEN <dhjeon1993@gmail.com>
2021-05-04 15:21:28 +0900

    create review module

---
+++ microservices/review/src/main/java/jeon/donghoon/microreview/MicroReviewApplication.java
@@ -0,0 +1,13 @@
+package jeon.donghoon.microreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroReviewApplication.class, args);
+	}
+
+}
---
+++ microservices/review/src/test/java/jeon/donghoon/microreview/MicroReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package jeon.donghoon.microreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
