fc1c6a6c0b0f32a979b21e847ef8a0a835e38169
DONGHOON JOEN <dhjeon1993@gmail.com>
2021-05-04 14:49:17 +0900

    initial commit

---
+++ src/test/java/jeon/donghoon/microdemo/MicroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jeon.donghoon.microdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
